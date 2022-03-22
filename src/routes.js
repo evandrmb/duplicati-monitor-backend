2f58a4c7762504ae9ff7269f6ed1d60f79c7c19d
Evandro Junior <evandrmb@gmail.com>
2022-03-22 16:02:09 -0300

    feat: added new routes related to Backup

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 const ReportsController = require('./app/controllers/ReportsController');
+const BackupsController = require('./app/controllers/BackupsController');
 
 const router = Router();
 
-router.post('/', ReportsController.store);
-router.get('/', ReportsController.index);
-router.delete('/', ReportsController.deleteAll);
+router.post('/', BackupsController.store);
+router.get('/', BackupsController.index);
+
+router.post('/:backupId', ReportsController.store);
+router.get('/:backupId', ReportsController.index);
+router.delete('/:backupId', ReportsController.deleteAll);
 
 module.exports = router;
